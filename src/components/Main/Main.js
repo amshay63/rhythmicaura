42676a0edb250c8f725ceebf020be5ed3fc7586f
Amshay63 <amshaybusiness@gmail.com>
2021-03-24 17:46:51 -0400

    Create React App and Publish to Github

---
+++ src/components/Main/Main.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+
+var background = {
+    height: '100vh',
+    background: 'linear-gradient(180deg, #3f9ab9 0%, #493d69 100%)'
+}
+
+function Main() {
+    return (
+        <div style={background}>
+            <h1></h1>
+        </div>
+    )
+}
+
+export default Main
---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: 'index.html',
+        cName: 'nav-links'
+    },
+    {
+        title: 'About',
+        url: 'about.html',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Music',
+        url: 'music.html',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Gallery',
+        url: 'gallery.html',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: 'contact.html',
+        cName: 'nav-links'
+    }
+]
